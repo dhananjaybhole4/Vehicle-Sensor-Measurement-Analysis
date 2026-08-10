143d14fabed69708710ea95ce6799438f952aab3
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-08-10 19:11:46 +0530

    removed the extractor method from anomaly detector class

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -6,22 +6,17 @@ from sklearn.ensemble import IsolationForest
 class AnomalyDetector():
     """Identifies anomaly from the data with the help of algorithm Isolation Forest
     """
-    def __init__(self):
-        None
-
-    # extracting data from csv using pandas
-    def csv_extractor(self, csv_path):
-        df = pd.read_csv(csv_path)
-        return df
+    def __init__(self, contamination = 0.05 ,random_state = 42):
+        self.contamination = contamination
+        self.randome_state = random_state
 
     # finding anomalies with the help of Isolation Forest
-    def anomaly_detector(self, csv_path):
+    def detect(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        df = self.csv_extractor(csv_path)
         # removed the timeframe column
         df_new = df.iloc[:,1:]
 
-        model = IsolationForest(contamination = 0.05, random_state = 42)
+        model = IsolationForest(contamination = self.contamination, random_state = self.randome_state)
         model.fit(df_new)
         predictions = model.predict(df_new)
 
