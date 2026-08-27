51a7ecf72be120ef74623b1f3af851a4727b4492
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-08-27 02:29:31 +0200

    Add logging config passed as a argument

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from anomaly_detector import AnomalyDetector
-from ingest import DataLoader
-from anomaly_report import AnomalyReport
+from src.anomaly_detector import AnomalyDetector
+from src.ingest import DataLoader
+from src.anomaly_report import AnomalyReport
 
 from pathlib import Path
 import argparse
+import logging
 
 # path variables
 path = Path.cwd()/"dataset/10.35097-1130/data/dataset/OBD-II-Dataset"
+
+logging_dict = {"debug": logging.DEBUG,
+                "info": logging.INFO,
+                "warning": logging.WARNING,
+                "error": logging.ERROR,
+                "fatal": logging.FATAL}
 
 def main(path):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("file_name")
     parser.add_argument("save_file_name")
+    parser.add_argument("--logging", default = "warning")
 
     args = parser.parse_args()
+
+    # logging
+    logging.basicConfig(level = logging_dict[(args.logging).lower()], format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s")
 
     # extract data from csv
     dataloader = DataLoader()
