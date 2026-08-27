3b5b5d34c31e16d541a31cda18d23b087f4b72f2
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-08-27 02:24:05 +0200

    added logging in dataloader file

--- src/ingest.py
+++ src/ingest.py
@@ -1,12 +1,32 @@
 import pandas as pd
 from pathlib import Path
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class DataLoader():
     def __init__(self):
         pass
 
     # extracting data from csv file
     def csv_extractor(self, path: Path) -> pd.DataFrame:
-        df = pd.read_csv(path)
-        return df
-            
+        try:
+            df = pd.read_csv(path)
+
+            # logging
+            logger.debug("columns: %s, shape: %s", df.columns.tolist(), df.shape)
+            logger.info("data successfully loaded with %s rows", df.shape[0])
+
+            if df.empty:
+                logger.warning("csv is empty")
+            elif df.isna().any().any():
+                logger.warning("%s data points are missing in csv", df.isna().sum().sum())
+
+            return df
+        except FileNotFoundError:
+            logger.error("given csv file doesnt exist")
+            raise FileNotFoundError("csv file doesnt exist")
+        except pd.errors.ParserError:
+            logger.error("given csv is corrupt")
+            raise ValueError("csv is corrupt")
