b22f4795359cc7d69803ba92466567d064f1aa97
Dhananjay Bhole <dhananjaybhole02@gmail.com>
2026-09-11 09:33:01 +0200

    import AnomalyExplainer class, dotenv, os modules and load the environment

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from src.anomaly_detector import AnomalyDetector
 from src.ingest import DataLoader
 from src.anomaly_report import AnomalyReport
+from src.anomaly_explainer import AnomalyExplainer
 
 from pathlib import Path
 import argparse
 import logging
+
+# API calling
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # path variables
 dataset_path = Path(__file__).parent/"dataset/10.35097-1130/data/dataset/OBD-II-Dataset"
@@ -36,10 +43,17 @@ def main(path):
     # get anamomalies using the defined Anomaly Detector class
     anomaly_detector = AnomalyDetector()
     anomalies = anomaly_detector.detect(df)
+    print(f"total anomalies capture:{anomalies["Time"].shape}")
 
     # get report
     anomaly_report = AnomalyReport()
-    anomaly_report.report(df, anomalies, report_path, args.save_file_name)
+    filtered_dataframe = anomaly_report.report(df, anomalies, report_path, args.save_file_name)
+
+    # get a summary about the anomalies from AI
+    api_key = os.environ["GEMINI_API_KEY"]
+    anomaly_explainer = AnomalyExplainer(api_key)
+    summary = anomaly_explainer.ai_explainer(filtered_dataframe, args.file_name)
+    print(summary)
 
 if __name__ == "__main__":
     main(dataset_path)
